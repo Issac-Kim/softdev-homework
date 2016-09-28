5798a46f57863a4ffc7aa5dfe491e0bd7360c603
Issac Kim <ikim1@stuy.edu>
2016-09-27 21:57:57 -0400

    change to flask struct

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
-from occupations import random_job, get_dict
+from utils import occupations
 
 app = Flask(__name__)
 
-d = get_dict()
+d = occupations.get_dict()
 
 
 @app.route("/")
 def welcome():
     return "Welcome!"
 
-@app.route("/occupations")
+@app.route("/occupations/")
 def webpage():
-    job = random_job()
+    job = occupations.random_job()
     return render_template('main.html', keys = d, job = job, percentage = d[job][0], link = d[job][1])
 if __name__ == '__main__':
     app.debug = True
